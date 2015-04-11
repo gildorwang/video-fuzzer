775e99f64a5f80e441c9d5a0f40ed87b377d4c8b
Gildor <gildorwang@gmail.com>
2015-04-11 13:11:55 +0800

    Initial version

---
+++ VideoFuzzer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VideoFuzzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(!args.Any())
+            {
+                return;
+            }
+            try
+            {
+                using(var stream = File.Open(args[0], FileMode.Append, FileAccess.Write))
+                {
+                    stream.Write(Enumerable.Repeat((byte)0, 16).ToArray(), 0, 16);
+                }
+            }
+            catch(Exception exception)
+            {
+                MessageBox.Show(exception.ToString());
+            }
+        }
+    }
+}
